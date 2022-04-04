f76c4f2ccebc8e2e76243174fd94732e8113786f
smartx-usman <usman@smartx.kr>
2022-04-04 13:01:38 +0200

    Change logging level.

--- src/main/java/org/telegraf/datastores/StoreRecordES.java
+++ src/main/java/org/telegraf/datastores/StoreRecordES.java
@@ -1,51 +1,66 @@
 package org.telegraf.datastores;
 
+import co.elastic.clients.elasticsearch.ElasticsearchClient;
+import co.elastic.clients.elasticsearch.core.IndexRequest;
+import co.elastic.clients.elasticsearch.indices.CreateIndexRequest;
+import co.elastic.clients.json.JsonData;
+import co.elastic.clients.json.jackson.JacksonJsonpMapper;
+import co.elastic.clients.transport.ElasticsearchTransport;
+import co.elastic.clients.transport.rest_client.RestClientTransport;
 import org.apache.http.HttpHost;
-import org.elasticsearch.action.index.IndexRequest;
-import org.elasticsearch.action.index.IndexResponse;
-import org.elasticsearch.client.RequestOptions;
 import org.elasticsearch.client.RestClient;
-import org.elasticsearch.client.RestHighLevelClient;
-//import org.elasticsearch.common.xcontent.XContentType;
-import org.elasticsearch.xcontent.XContentType;
 
 import java.io.IOException;
+import java.io.InputStream;
 import java.util.Map;
 
 public class StoreRecordES implements storable {
-    RestHighLevelClient client;
+    ElasticsearchClient client;
+    IndexRequest<JsonData> req;
 
     public StoreRecordES() {
         this.set_client();
     }
 
     public void set_client() {
-        client = new RestHighLevelClient(
-                RestClient.builder(
-                        new HttpHost("es-master-0.es-master-headless.monitoring.svc.cluster.local", 9200, "http")));
+        // Create the low-level client
+        RestClient restClient = RestClient.builder(
+                new HttpHost("es-master-0.es-master-headless.monitoring.svc.cluster.local", 9200)).build();
+
+        // Create the transport with a Jackson mapper
+        ElasticsearchTransport transport = new RestClientTransport(
+                restClient, new JacksonJsonpMapper());
+
+        // And create the API client
+        ElasticsearchClient client = new ElasticsearchClient(transport);
     }
 
     public void close_client() {
-        try {
-            client.close();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        //client. close();
+        System.out.println("Close connection.");
     }
 
     @SuppressWarnings({})
     @Override
     public void store_record(String ES_Index, Map<String, Object> record) {
-        IndexRequest request = new IndexRequest(ES_Index);
-        request.source(record, XContentType.JSON);
+        CreateIndexRequest request = CreateIndexRequest.of(b -> b
+                .index(ES_Index));
 
-        IndexResponse response = null;
         try {
-            response = client.index(request, RequestOptions.DEFAULT);
+            boolean created = Boolean.TRUE.equals(client.indices().create(request).acknowledged());
         } catch (IOException e) {
             e.printStackTrace();
         }
-        String index = response.getIndex();
-        long version = response.getVersion();
+
+        req = IndexRequest.of(b -> b
+                .index(ES_Index)
+                .withJson((InputStream) record)
+        );
+
+        try {
+            client.index(req);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }

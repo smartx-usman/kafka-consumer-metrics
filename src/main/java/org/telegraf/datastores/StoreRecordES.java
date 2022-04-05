71adc9376192a458dee55e4d33425b808a102318
smartx-usman <usman@smartx.kr>
2022-04-05 13:00:43 +0200

    Change logging level.

--- src/main/java/org/telegraf/datastores/StoreRecordES.java
+++ src/main/java/org/telegraf/datastores/StoreRecordES.java
@@ -1,30 +1,22 @@
 package org.telegraf.datastores;
 
 import co.elastic.clients.elasticsearch.ElasticsearchClient;
-//import co.elastic.clients.elasticsearch.core.ExistsRequest;
-import co.elastic.clients.elasticsearch.indices.ExistsRequest;
 import co.elastic.clients.elasticsearch.core.IndexRequest;
 import co.elastic.clients.elasticsearch.indices.CreateIndexRequest;
+import co.elastic.clients.elasticsearch.indices.ExistsRequest;
 import co.elastic.clients.elasticsearch.indices.GetIndexRequest;
-import co.elastic.clients.elasticsearch.indices.GetIndexResponse;
 import co.elastic.clients.json.JsonData;
 import co.elastic.clients.json.jackson.JacksonJsonpMapper;
 import co.elastic.clients.transport.ElasticsearchTransport;
 import co.elastic.clients.transport.endpoints.BooleanResponse;
 import co.elastic.clients.transport.rest_client.RestClientTransport;
-import co.elastic.clients.util.ObjectBuilder;
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.apache.http.HttpHost;
-import org.elasticsearch.client.RequestOptions;
 import org.elasticsearch.client.RestClient;
-import org.json.simple.JSONObject;
 
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.Map;
-import java.util.function.Function;
 
 public class StoreRecordES implements storable {
     ElasticsearchClient client;
@@ -63,18 +55,15 @@ public class StoreRecordES implements storable {
                     .index(ES_Index));
 
 
-
             //GetIndexResponse indexResponse = client.indices().get(getIndex);
 
             ExistsRequest checkIndex = ExistsRequest.of(b -> b
                     .index(ES_Index));
             BooleanResponse indexExists = client.indices().exists(checkIndex);
 
-            System.out.println(indexExists);
+            System.out.println(indexExists.value());
 
-            boolean exists=false;
-            if (indexExists.value())
-            {
+            if (!indexExists.value()) {
                 boolean created = client.indices().create(request).acknowledged();
                 System.out.println("Index is created.");
             }

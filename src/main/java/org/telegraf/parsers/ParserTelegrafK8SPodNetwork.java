9e067faf2eb38e5532c22e22c4c24ecf4c36e215
smartx-usman <usman@smartx.kr>
2023-02-03 18:35:06 +0100

    Save data as json for external sharing.

--- src/main/java/org/telegraf/parsers/ParserTelegrafK8SPodNetwork.java
+++ src/main/java/org/telegraf/parsers/ParserTelegrafK8SPodNetwork.java
@@ -1,11 +1,17 @@
 package org.telegraf.parsers;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SequenceWriter;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.telegraf.datastores.StoreRecordES;
 import org.telegraf.datastores.StoreRecordPrometheus;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.Files;
 import java.time.Instant;
 import java.util.HashMap;
 import java.util.Map;
@@ -13,12 +19,24 @@ import java.util.Map;
 
 public class ParserTelegrafK8SPodNetwork implements parsable {
     private static final Logger logger = LogManager.getLogger(ParserTelegrafK8SPodNetwork.class);
+
     private final StoreRecordES store_record_es;
     private final StoreRecordPrometheus store_record_prometheus;
+    private final File file = new File("/metrics/kubernetes_pod_network.json");
+    private final ObjectMapper mapper = new ObjectMapper();
+    private FileWriter file_writer = null;
+    private SequenceWriter sequence_writer = null;
 
     public ParserTelegrafK8SPodNetwork(StoreRecordES es, StoreRecordPrometheus prometheus) {
         store_record_es = es;
         store_record_prometheus = prometheus;
+        try {
+            Files.deleteIfExists(file.toPath());
+            file_writer = new FileWriter(file, true);
+            sequence_writer = mapper.writer().writeValuesAsArray(file_writer);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
@@ -35,7 +53,6 @@ public class ParserTelegrafK8SPodNetwork implements parsable {
 
             String[] host_label = measurement_plugin_labels[1].split("=");
             String[] namespace = measurement_plugin_labels[2].split("=");
-            String[] node_name = measurement_plugin_labels[3].split("=");
             String[] pod_name = measurement_plugin_labels[4].split("=");
 
 
@@ -57,10 +74,21 @@ public class ParserTelegrafK8SPodNetwork implements parsable {
                 jsonMap.put(label_and_value[0], label_and_value[1]);
             }
 
+            sequence_writer.write(jsonMap);
             store_record_es.store_record(es_index, jsonMap);
         } catch (Exception e) {
             store_record_es.close_client();
             e.printStackTrace();
+        }
+    }
+
+    @Override
+    protected void finalize() {
+        try {
+            file_writer.close();
+            sequence_writer.close();
+        } catch (IOException e) {
+            logger.error("Error in closing file writer.", e);
         }
     }
 }
